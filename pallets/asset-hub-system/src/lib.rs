9c7fe5a6c70cfdc079a39a301a9075d87b9bf4d7
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2026-07-27 13:48:38 +0300

    stub pallets
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

---
+++ pallets/asset-hub-system/src/lib.rs
@@ -0,0 +1,23 @@
+//! Asset Hub System Pallet to exercise asset-hub specific host functions and hooks.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame::pallet]
+pub mod pallet {
+	use frame::prelude::*;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {}
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+        fn on_initialize(_n: BlockNumberFor<T>) -> Weight {
+            todo!()
+        }
+    }
+}
